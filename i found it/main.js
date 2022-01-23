2a4619ad95e219ead33b3481f3ad20dab36d9104
stet345434543454345 <83256244+stet345434543454345@users.noreply.github.com>
2022-01-23 05:39:49 -0800

    Add files via upload

---
+++ i found it/main.js
@@ -0,0 +1,28 @@
+
+function preload() {
+    video = createVideo('video.mp4');
+    video.hide();
+}
+
+function setup() {
+    canvas =  createCanvas(480, 380);
+	canvas.center();
+
+}
+function draw() {
+    image(video, 0, 0, 480, 380);
+
+}
+function start() {
+
+    objectdetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "status : obdj dect";
+}
+function modelLoaded() {
+    console.log("mole");
+    status = "true";
+    video.loop();
+    video.speed(1);
+    video.volume(100);
+
+}
